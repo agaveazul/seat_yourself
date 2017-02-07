6fc447895543d34900b071685140afd430b7435a
Richard Strauss <rstrauss91@gmail.com>
2017-02-07 18:26:36 -0500

    Update associations from owners to users

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,9 @@
 class Restaurant < ApplicationRecord
   has_many :reservations
-  has_many :customers, through: :reservations, class_name: "User"
+
+  # suggestion: just call these "users"
+  # has_many :customers, through: :reservations, class_name: "User", source: :user
+
+  has_many :users, through: :reservations
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
 end
