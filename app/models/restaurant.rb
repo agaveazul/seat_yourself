70db3a5e0acb32bbbb666e6ea589013b0e9a7357
Richard Strauss <rstrauss91@gmail.com>
2017-02-07 17:35:30 -0500

    Update association for foreign_key

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :reservations
   has_many :customers, through: :reservations, class_name: "User"
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: "User", foreign_key: "user_id"
 end
