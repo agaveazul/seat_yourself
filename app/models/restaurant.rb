9019a9d8eb9cc7b16ad53732cc538b1f47489af0
Richard Strauss <rstrauss91@gmail.com>
2017-02-07 18:40:13 -0500

    Update user association between Restaurant -> User through Reservations to be called customers

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,8 +2,8 @@ class Restaurant < ApplicationRecord
   has_many :reservations
 
   # suggestion: just call these "users"
-  # has_many :customers, through: :reservations, class_name: "User", source: :user
+  # has_many :users, through: :reservations
+  has_many :customers, through: :reservations, source: :user
 
-  has_many :users, through: :reservations
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :reservations
-  has_many :reserved_restaurants, through: :reservations, class_name: "Restaurant", source: :restaurant
+  has_many :reserved_restaurants, through: :reservations, source: :restaurant
   has_many :owned_restaurants, class_name: "Restaurant"
 end
