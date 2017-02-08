28340df25534d00ebc457959ec9ebb2dddbfa47d
Akshit Sahani <akshit_sahani@hotmail.com>
2017-02-08 16:36:23 -0500

    removed bullshit files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to products_url, notice: "Logged in!"
+      redirect_to restaurants_path, :notice => "Successfully logged in!"
     else
+      flash.now[:alert] = "Invalid email or password"
       render "new"
     end
   end
@@ -16,5 +17,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to products_url, notice: "Logged out!"
   end
-  
+
 end
