eee1e403155a016fda22bf0daaaaec6601730a42
Richard Strauss <rstrauss91@gmail.com>
2017-02-08 12:12:43 -0500

    Change belong to association in reservation to refer to customer and reserved_restaurant instead of user and restaurant to be consistent with other through associations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,4 @@
 class Reservation < ApplicationRecord
-  belongs_to :user
-  belongs_to :restaurant
+  belongs_to :customer, class_name: "User", foreign_key: "user_id"
+  belongs_to :reserved_restaurant, class_name: "Restaurant", foreign_key: "restaurant_id"
 end
