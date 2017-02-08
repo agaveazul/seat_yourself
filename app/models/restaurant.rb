421dd7c5b13e4122db549616c0c0dbde679635ef
Richard Strauss <rstrauss91@gmail.com>
2017-02-08 17:54:36 -0500

    Create restaurant show.html.erb + controller logic

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,4 +6,16 @@ class Restaurant < ApplicationRecord
   has_many :customers, through: :reservations, source: :user
 
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
+
+def timeslot_check
+
+# go into a day / time combo
+# add up all of the current reservations that exist
+# compare against the current capacity that was set
+# if result > 0, then display [Select]
+# if result < 10, then display "#{result} seats left"
+
+
+end
+
 end
