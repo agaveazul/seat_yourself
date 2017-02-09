dcd1c526722747905bdfd7e55108ec238df32d2a
Richard Strauss <rstrauss91@gmail.com>
2017-02-09 13:10:11 -0500

    Create logic for creating and saving reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,14 @@
 class ReservationsController < ApplicationController
 
 def create
+
   @reservation = Reservation.new(reservation_params)
   if @reservation.save
+    flash[:alert] = reservation_params
     redirect_to restaurants_path
   else
-    redirect_to "restaurants/#{params[:restaurant_id]}"
+    flash[:alert] = reservation_params
+    redirect_to "/restaurants/#{params[:restaurant_id]}"
   end
 
 end
@@ -13,7 +16,7 @@ end
 
 private
 def reservation_params
-  params.require(:reservation).permit(:party_size, :timeslot, :restaurant_id)
+  params.require(:reservation).permit(:party_size, :timeslot, :restaurant_id, :user_id, :date)
 end
 
 end
