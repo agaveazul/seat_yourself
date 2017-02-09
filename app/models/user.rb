f265344272fe548ead3969b49ff933545ba9ad1a
Akshit Sahani <akshit_sahani@hotmail.com>
2017-02-09 14:02:13 -0500

    User validations all work

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :owned_restaurants, class_name: "Restaurant"
   has_secure_password
 
+  validates :name, :phone_number, :email, :password, :password_confirmation, presence: true
+  validates :phone_number, numericality: {only_integer: true}
+  validates :phone_number, :email, uniqueness: true
+  validates :password, confirmation: true
+
 end
