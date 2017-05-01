2992ae7928ca5ee0d4acd3c2e4c615b3c605c103
Akshit Sahani <akshit_sahani@hotmail.com>
2017-05-01 14:55:02 -0400

    tryin to deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170207220916) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "restaurant_id"
